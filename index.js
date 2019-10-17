0af376bf65a86fe7cfe5a015280fa813ba2188bc
apiaoqzh <qiuzehang@inrice.com>
2019-10-17 18:01:20 +0800

    feat: 开荒

---
+++ index.js
@@ -0,0 +1,53 @@
+export default class Captcha {
+  constructor (options) {
+    if (!window.TencentCaptcha) {
+      require('./lib/tcaptcha')
+    }
+    this.o = {
+      appId: options.appId
+    }
+    this.captcha = null
+    if (!this.o.appId) {
+      throw new Error('请填写appId')
+    }
+  }
+  showCaptcha () {
+    let id = (new Date()).getTime()
+    return new Promise((resolve, reject) => {
+      console.log(this)
+      this.captcha = new window.TencentCaptcha(this.o.appId, function(res) {
+        if (res.ret > 0) {
+          // 用户自行关闭
+          let err = new Error('用户自行关闭')
+          err.res = res
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      }, { bizState: id })
+      this.captcha.show()
+    })
+  }
+  closeCaptcha () {
+    return new Promise((resolve, reject) => {
+      if (this.captcha) {
+        this.captcha.destroy()
+        resolve()
+      } else {
+        let err = new Error('没有实例')
+        reject(err)
+      }
+    })
+  }
+  getTicket () {
+    return new Promise((resolve, reject) => {
+      if (this.captcha) {
+        let res = this.captcha.getTicket()
+        resolve(res)
+      } else {
+        let err = new Error('没有实例')
+        reject(err)
+      }
+    })
+  }
+}
