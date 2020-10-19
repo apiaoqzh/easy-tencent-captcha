c36808fc3d7dac86d973b5dcd2a8a600e02e1646
apiao <qiuzehang@inrice.com>
2020-10-19 17:08:29 +0800

    feat: publish 1.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard',
+    'plugin:sonarjs/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'sonarjs'
+  ],
+  // add your custom rules here
+  rules: {
+    'eol-last': 0,
+    'no-new': 'off',
+    'no-multiple-empty-lines': 1,
+    'arrow-parens': 0,
+    'camelcase': 0,
+    'no-tabs': 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 1,
+    // 禁止重复 import
+    "space-before-function-paren": ["off", "always"]
+  },
+  globals: { MobileJS: true }
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'easy-tencent-captcha.min.js',
+    library: 'EasyCaptcha', // 指定类库名,主要用于直接引用的方式(比如script)
+    libraryExport: 'default', // 对外暴露default属性，就可以直接调用default里的属性
+    globalObject: 'this', // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
+    libraryTarget: 'umd' // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
+  },
+  mode: 'production', // 告诉webpack使用production模式的内置优化,
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: /(node_modules|bower_components)/,
+      loader: 'eslint-loader',
+      enforce: 'pre',
+      options: {
+        formatter: require('eslint-friendly-formatter'),
+        quiet: true
+      }
+    }, {
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+  ],
+  externals: { // 从输出的bundle中排除依赖
+  }
+}
